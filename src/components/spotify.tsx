9bf7ed6c06a049bd4cd7d9f3ca7b780a90c60fb9
ImMaxM <max@maxuk.me>
2023-10-26 00:05:02 +0100

    feat: lanyard

--- src/components/spotify.tsx
+++ src/components/spotify.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+import { Spotify, useLanyardWS } from "use-lanyard";
+
+const userId = "866686984587313173";
 
 const Spotify = () => {
+  const [spotifyData, setSpotifyData] = useState<Spotify | null>(null);
+
+  const data = useLanyardWS(userId);
+
+  useEffect(() => {
+    if (data?.spotify) {
+      setSpotifyData(data.spotify);
+    }
+  }, [data]);
+
+  console.log(spotifyData);
+
   return (
     <div>
-      <p>Spotify song data here!</p>
+      {spotifyData ? (
+        <div>
+          <p>
+            {spotifyData.song}; {spotifyData.artist}
+          </p>
+        </div>
+      ) : (
+        <p>Loading spotify song data...</p>
+      )}
     </div>
   );
 };
