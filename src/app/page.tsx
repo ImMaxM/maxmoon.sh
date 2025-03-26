8ce50b58872ad81f2d8f97745b8a501535662753
Max <max@maxuk.me>
2025-03-26 09:57:17 +0000

    feat: plausible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,7 @@ export default function Home() {
 
   return (
     <main className="min-h-screen text-white">
-      <script
-        defer
-        src="https://web.maxmoon.sh/script.js"
-        data-website-id="93fdf6d3-da9a-4f58-ac94-6e0536673641"
-      ></script>
+     <script defer data-domain="maxmoon.sh" src="https://web.maxmoon.sh/js/script.outbound-links.js"></script>
       <div className="mx-auto max-w-3xl py-12">
         <Hero isVisible={isLoaded} delay={0} />
         <div className="px-8">
